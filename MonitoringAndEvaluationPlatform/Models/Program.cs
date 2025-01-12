60b0bf77b8f664ad339795f368e15d33d784a9b3
hamzasukkar1989 <89667360+hamzasukkar1989@users.noreply.github.com>
2025-01-12 21:04:11 +0200

    #12 Programs

--- MonitoringAndEvaluationPlatform/Models/Program.cs
+++ MonitoringAndEvaluationPlatform/Models/Program.cs
@@ -10,6 +10,16 @@ namespace MonitoringAndEvaluationPlatform.Models
         public double EstimatedBudget { get; set; }
         public double RealBudget { get; set; }
         public string Trend { get; set; }
+        public string ProjectManager { get; set; }
+        public string SuperVisor { get; set; }
+        public string Type { get; set; }
+        public string Status1 { get; set; }
+        public string Status2 { get; set; }
+        public string Category { get; set; }
+        public string Donor { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Region { get; set; }
         public int performance { get; set; }
         public int DisbursementPerformance { get; set; }
         public int FieldMonitoring { get; set; }
