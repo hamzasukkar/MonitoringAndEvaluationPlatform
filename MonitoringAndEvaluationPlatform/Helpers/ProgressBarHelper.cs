d197045efee26d165812900d720b4f54307f8899
hamzasukkar1989 <89667360+hamzasukkar1989@users.noreply.github.com>
2025-02-10 21:59:02 +0200

    Progress Chart Color #22

---
+++ MonitoringAndEvaluationPlatform/Helpers/ProgressBarHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace MonitoringAndEvaluationPlatform.Helpers
+{
+    public static class ProgressBarHelper
+    {
+        public static string GetProgressBarClass(int value)
+        {
+            return value switch
+            {
+                > 80 => "progress-bar-success",
+                > 60 => "progress-bar-yellow",
+                > 40 => "progress-bar-goldenYellow",
+                > 20 => "progress-bar-orange",
+                _ => "progress-bar-danger"
+            };
+        }
+
+        public static string GetTrendImage(double trend)
+        {
+            return trend switch
+            {
+                >= 80 => "external-link-success.png",
+                >= 50 => "external-link-info.png",
+                >= 30 => "external-link-squared.png",
+                _ => "external-link-danger.png"
+            };
+        }
+    }
+
+}
